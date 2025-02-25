242cdd094d1de71556f1915e1481fa819dfce803
jigglewiggle101 <s10262431@connect.np.edu.sg>
2025-02-25 12:49:22 +0800

    updated all

--- my-next-app/app/components/spinner.tsx
+++ my-next-app/app/components/spinner.tsx
@@ -2,7 +2,7 @@
 const Spinner = () => (
     <div className="flex justify-center items-center">
       <img 
-        src="/public/4sep24.jpg" 
+        src="/4sep24.jpg" 
         alt="Spinning Image" 
         className="w-32 h-32 rounded-full animate-spin-slow" 
       />
--- my-next-app/app/layout.tsx
+++ my-next-app/app/layout.tsx
@@ -1,38 +1,39 @@
 // app/layout.tsx
 import Link from 'next/link';
-import '../styles/globals.css';
+import '../app/globals.css';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <header className="bg-blue-800 text-white p-4 text-center">
-        <h1>Happy Birthday, Shawn Seah Yao Ting!</h1>
-      </header>
-      
-      {/* Navbar for Navigation */}
-      <nav className="bg-blue-600 text-white p-4">
-        <div className="flex justify-center space-x-8">
-          <Link href="/" className="text-lg hover:underline">
-            Home
-          </Link>
-          <Link href="/quiz" className="text-lg hover:underline">
-            Quiz
-          </Link>
-          <Link href="/memory-lane" className="text-lg hover:underline">
-            Memory Lane
-          </Link>
-          {/* Add more links as needed */}
-        </div>
-      </nav>
-      
-      {/* Main content */}
-      <main>{children}</main>
+    <html lang="en">
+      <head />
+      <body>
+        <header className="bg-blue-800 text-white p-4 text-center">
+          <h1>Happy Birthday, Shawn Seah Yao Ting!</h1>
+        </header>
+        
+        {/* Navbar for Navigation */}
+        <nav className="bg-blue-600 text-white p-4">
+          <div className="flex justify-center space-x-8">
+            <Link href="/" className="text-lg hover:underline">
+              Home
+            </Link>
+            <Link href="/quiz" className="text-lg hover:underline">
+              Quiz
+            </Link>
+            <Link href="/memory-lane" className="text-lg hover:underline">
+              Memory Lane
+            </Link>
+            {/* Add more links as needed */}
+          </div>
+        </nav>
+        
+        {/* Main content */}
+        <main>{children}</main>
 
-      <footer className="bg-blue-800 text-white p-4 text-center">
-        <p>Made with ❤️</p>
-      </footer>
-    </div>
+        <footer className="bg-blue-800 text-white p-4 text-center">
+          <p>Made with ❤️</p>
+        </footer>
+      </body>
+    </html>
   );
-};
-
-export default Layout;
+}
