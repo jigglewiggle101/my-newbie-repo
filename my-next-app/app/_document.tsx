5fdb85d8851ad10dd0e86536dd1c989abd5b77f0
jigglewiggle101 <s10262431@connect.np.edu.sg>
2025-02-26 19:12:31 +0800

    Fix build and add next.js to dependencies

---
+++ my-next-app/app/_document.tsx
@@ -0,0 +1,16 @@
+// app/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const MyDocument = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MyDocument;
--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,  // Ensures stricter checks for potential issues during development.
 };
 
 export default nextConfig;
